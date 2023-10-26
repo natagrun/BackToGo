906532ee7ec997e23738244629c02b3f1708bede
Nata <ngrunskaya1994@gmail.com>
2023-10-26 22:16:11 +0300

    fixed version

--- main.go
+++ main.go
@@ -1,38 +1,35 @@
-package main
+package linkedList
 
 import (
 	"fmt"
 )
 
-type Node struct {
+type node struct {
 	val  int
-	next *Node
+	next *node
 }
 
 type LinkedList struct {
-	head *Node
+	head *node
 	len  int
 }
 
 // CreateNode создание нового узла
-func CreateNode(val int) *Node {
-	node := Node{val, nil}
+func CreateNode(val int) *node {
+	node := node{val, nil}
 	return &node
 }
 
 // New создание нового связного списка длины q
 func New(q int) *LinkedList {
 	list := LinkedList{CreateNode(0), q}
-	if q > 1 {
-		temp := list.head
-		for i := 1; i < q; i++ {
-			temp.next = CreateNode(0)
-			temp = temp.next
-		}
+	temp := list.head
+	for i := 1; i < q; i++ {
+		temp.next = CreateNode(0)
+		temp = temp.next
 	}
 	list.len = q
 	return &list
-
 }
 
 // Add добавление нового узла в конец списка
@@ -41,33 +38,33 @@ func (l *LinkedList) Add(val int) {
 		head := CreateNode(val)
 		l.head = head
 		l.len = 1
-	} else {
-		temp := l.head
-		for temp.next != nil {
-			temp = temp.next
-		}
-		temp.next = CreateNode(val)
-		l.len++
+		return
 	}
+	temp := l.head
+	for temp.next != nil {
+		temp = temp.next
+	}
+	temp.next = CreateNode(val)
+	l.len++
 }
 
 // Pop удаление последнего элемента списка
 func (l *LinkedList) Pop() {
-	if l.len == 0 {
+	switch {
+	case l.len == 0:
 		fmt.Println("Empty list")
-	} else {
-		if l.len == 1 {
-			l.head = nil
-			l.len = 0
-		} else {
-			temp := l.head
-			for temp.next.next != nil {
-				temp = temp.next
-			}
-			temp.next = nil
-			l.len--
-		}
+		return
+	case l.len == 1:
+		l.head = nil
+		l.len = 0
+		return
 	}
+	temp := l.head
+	for temp.next.next != nil {
+		temp = temp.next
+	}
+	temp.next = nil
+	l.len--
 }
 
 // At вывод числа соответсвующего номеру pos в списке
@@ -87,26 +84,20 @@ func (l *LinkedList) Size() int {
 // DeleteFrom удаление элемента с места pos
 func (l *LinkedList) DeleteFrom(pos int) {
 	temp := l.head
-	switch true {
-	case pos > l.len || pos < 1:
+	if pos > l.len || pos < 1 {
 		fmt.Println("wrong enter")
-
-	case pos == 1: //если нужно удалить первый узел списка
-		l.head = l.head.next
-
-	case pos == l.len: //если удаляется последний узел списка
-		for temp.next.next != nil {
-			temp = temp.next
-		}
-		temp.next = nil
-
-	default:
-		for i := 1; i < pos-1; i++ {
-			temp = temp.next
-		}
-		temp.next = temp.next.next
+		return
 	}
 	l.len--
+	if pos == 1 {
+		l.head = l.head.next
+		return
+	}
+	for i := 1; i < pos-1; i++ {
+		temp = temp.next
+	}
+	temp.next = temp.next.next
+
 }
 
 // UpdateAt обновление значения узла по его номеру в списке
@@ -123,11 +114,13 @@ func (l *LinkedList) UpdateAt(pos, val int) {
 
 // NewFromSlice создание нового списка из среза
 func NewFromSlice(s []int) *LinkedList {
-	list := New(len(s))
-	for i := 0; i < len(s); i++ {
-		list.UpdateAt(i+1, s[i])
+	list := LinkedList{CreateNode(s[0]), len(s)}
+	temp := list.head
+	for i := 1; i < len(s); i++ {
+		temp.next = CreateNode(s[i])
+		temp = temp.next
 	}
-	return list
+	return &list
 }
 
 // PrintList вывод листа на экран
